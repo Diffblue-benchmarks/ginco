d22157baa14dd97c594001533e503cba41d30f66
Thomas Perkins <thomasperkins1123@gmail.com>
2020-10-01 20:34:58 +0000

    Update for version 0.3 7c104c29 of jcover

--- ginco-impl/src/jcover/java/fr/mcc/ginco/services/ConceptHierarchicalRelationshipServiceUtilTest.java
+++ ginco-impl/src/jcover/java/fr/mcc/ginco/services/ConceptHierarchicalRelationshipServiceUtilTest.java
@@ -1,6 +1,8 @@
 package fr.mcc.ginco.services;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.when;
 
 import fr.mcc.ginco.beans.ThesaurusConcept;
 import fr.mcc.ginco.dao.IConceptHierarchicalRelationshipDAO;
@@ -8,10 +10,13 @@ import fr.mcc.ginco.dao.IThesaurusArrayConceptDAO;
 import fr.mcc.ginco.dao.IThesaurusConceptDAO;
 import fr.mcc.ginco.dao.IThesaurusTermDAO;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 /**
@@ -39,6 +44,13 @@ public class ConceptHierarchicalRelationshipServiceUtilTest {
 
 	@Before public void initMocks() {
 		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void calculateChildrenRoots() {
+		when(thesaurusConceptDAO.getChildrenConcepts(Mockito.<String>any(), anyInt(), Mockito.<String>any()))
+			.thenReturn(new ArrayList<ThesaurusConcept>());
+		service.calculateChildrenRoots("1234", "1234");
 	}
 
 	@Test
